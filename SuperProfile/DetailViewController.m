d106c6c92e99172e5ce1a584efad43f941f44cdb
Ken Tsutsumi <p27e4a@gmail.com>
2014-04-26 22:18:10 +0900

    Initial Commit

---
+++ SuperProfile/DetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.m
+//  SuperProfile
+//
+//  Created by Ken Tsutsumi on 2014/04/26.
+//  Copyright (c) 2014年 LvUP Inc. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+- (void)configureView;
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SuperProfile/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  SuperProfile
+//
+//  Created by Ken Tsutsumi on 2014/04/26.
+//  Copyright (c) 2014年 LvUP Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
